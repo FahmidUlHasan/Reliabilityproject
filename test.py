c8a17c762e57ce8885cc562e6d43bba1724c7186
FahmidUlHasan <fahmidpulok@gmail.com>
2023-11-27 01:10:36 +0600

    First release of Reliabilityproject

---
+++ test.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+# Define the target distribution function
+def target_distribution(x):
+    # Replace this with your specific expression
+    # For example: return 1 - np.exp(-x[0]**2 - x[1]**2 - ... - x[6]**2)
+    return np.prod(
+        1 - np.exp(-(x**2) / 2)
+    )  # Assuming each variable is standard normal
+
+
+# Define the proposal distribution function
+def proposal_distribution(x):
+    return np.prod(np.exp(-(x**2) / 2))  # Assuming each variable is standard normal
+
+
+# Number of samples
+num_samples = 100000
+
+# Initialize variables to store the results
+rare_event_count = 0
+estimated_probability = 0
+
+# Generate samples from the proposal distribution
+samples = np.random.normal(0, 1, size=(num_samples, 7))
+
+# Perform importance sampling
+for sample in samples:
+    weight = target_distribution(sample) / proposal_distribution(sample)
+    rare_event_count += np.prod(sample) <= 0
+    estimated_probability += (np.prod(sample)) * weight
+
+estimated_probability /= num_samples
+
+print("Estimated Probability:", estimated_probability)
+print("Rare Event Count:", rare_event_count)
+print("Hello world")
+print(samples.shape)
+print("whatever I am doing")
+print("creating merge conflict")
+print("Again Whatever I am doing")
